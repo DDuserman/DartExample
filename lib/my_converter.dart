23378958be09eed861e943ab72b795715018f9b7
examen <examen@CampusUninorte.local>
2023-08-02 11:40:38 -0500

    clase

--- lib/my_converter.dart
+++ lib/my_converter.dart
@@ -2,20 +2,41 @@ import 'Utils.dart';
 
 class MyConverter {
   // todo: Define dos varibles late privadas String [_binary] y [_decimal]
+  late String _binary;
+  late String _decimal;
 
   MyConverter() {
     // todo: Inicializa [_binary] y [_decimal] en ['0']
+    _binary = '0';
+    _decimal = '0';
   }
 
   // TODO: Crea un [setter] y [getter] para [binary] .
+  void set binary(String value) {
+    _binary = value;
+  }
+
+  String get binary {
+    return _binary;
+  }
 
   // TODO: Crea un [setter] y [getter] para [decimal].
 
+  void set decimal(String value) {
+    _decimal = value;
+  }
+
+  String get decimal {
+    return _decimal;
+  }
+
   String convertBinary() {
     // todo: Usa [Util] para convertir el numero binario a un numero decimal.
+    return _decimal = Utils.bin2dec(_binary);
   }
 
   String convertDecimal() {
     // todo: Usa [Converter] para convertir el numero decimal a un numero binario.
+    return _binary = Utils.dec2bin(_decimal);
   }
 }
